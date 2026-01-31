74d6d8833d713f77fad496acf6499df441335b57
shaylaromo07 <sarcclases@gmail.com>
2026-01-31 14:52:39 +0100

    Otro commit

---
+++ src/main/java/org/ies/tierno/readers/AudioReader.java
@@ -0,0 +1,24 @@
+package org.ies.tierno.readers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.ies.tierno.models.typefiles.AudioFile;
+import org.ies.tierno.models.typefiles.ImageFile;
+import java.util.Random;
+@Data
+@AllArgsConstructor
+public class AudioReader implements Reader<AudioFile> {
+    private final static String[] PATH = {"/Music","/Desktop/Audio/Whatshap","/Desktop/Audio/Edits","/Music/Records"};
+    private final static String[] LENGHT = {"9:30","2:03","4:50","1:00:43","0:20","4:09"};
+
+    private final Random random;
+
+    @Override
+    public AudioFile read() {
+        return new AudioFile(
+                random.nextInt(128),
+                PATH[random.nextInt(PATH.length)],
+                LENGHT[random.nextInt(LENGHT.length)]
+        );
+    }
+}
--- src/main/java/org/ies/tierno/readers/ImageReader.java
+++ src/main/java/org/ies/tierno/readers/ImageReader.java
@@ -1,5 +1,23 @@
 package org.ies.tierno.readers;
 
-public class ImageReader {
-    private
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.ies.tierno.models.typefiles.ImageFile;
+import java.util.Random;
+@Data
+@AllArgsConstructor
+
+public class ImageReader implements Reader<ImageFile> {
+    private final static String[] PATH = {"/Image","/Desktop/Imagenes/Tarea","/Desktop/Imagenes/Dibujos","/Image/CapturasDePantalla"};
+    private final Random random;
+
+    @Override
+    public ImageFile read() {
+        return new ImageFile(
+                random.nextInt(128),
+                PATH[random.nextInt(PATH.length)],
+                random.nextInt(100000),
+                random.nextInt(100000)
+        );
+    }
 }
