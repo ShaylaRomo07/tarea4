837eeb93b306c1cb601d03732acdbd6506033364
shaylaromo07 <sarcclases@gmail.com>
2026-01-31 15:27:50 +0100

    Otro commit

--- src/main/java/org/ies/tierno/Main.java
+++ src/main/java/org/ies/tierno/Main.java
@@ -7,16 +7,19 @@ import org.ies.tierno.readers.TextReader;
 
 import java.util.Properties;
 import java.util.Random;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         TextReader textReader = new TextReader(random);
         ProgramReader programReader = new ProgramReader<>(random, textReader);
         Program program = programReader.read();
-        System.out.println("");
-        program.execute();
+        System.out.println("Nuevo path");
+        String outputPath = scanner.nextLine();
+        program.execute(outputPath);
     }
 }
